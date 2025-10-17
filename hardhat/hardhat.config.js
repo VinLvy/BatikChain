2ce7dc3f7549b38c4f04077e0870dc045d610b2b
VinLvy <123059312+VinLvy@users.noreply.github.com>
2025-10-17 08:44:28 +0700

    Contracts 1.9

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -18,14 +18,17 @@ module.exports = {
         },
         sepolia: {
             url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
-            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+            chainId: 11155111,
+            gasPrice: 20000000000, // 20 gwei
+            gas: 8000000
         },
         amoy: {
             url: process.env.POLYGON_AMOY_URL || "https://rpc-amoy.polygon.technology/",
-            accounts: [process.env.PRIVATE_KEY],
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
         },
         polygon: {
-            url: "https://polygon-rpc.com",
+            url: process.env.POLYGON_RPC_URL || "https://polygon-rpc.com",
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
         }
     },
@@ -34,5 +37,12 @@ module.exports = {
         tests: "./test",
         cache: "./cache",
         artifacts: "./artifacts"
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.ETHERSCAN_API_KEY,
+            polygon: process.env.POLYGONSCAN_API_KEY,
+            polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+        }
     }
 };
